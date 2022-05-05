67d5f2f1e8a2130987704bffe58ec7861d4ab911
Morado <roberto@morado.dev>
2022-05-04 22:04:07 -0400

    typescript
    setup typescript and migrate files

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const div = document.getElementById('root');
+
+if (!!div) {
+  const root = ReactDOM.createRoot(div);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
